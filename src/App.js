972c7699d503cf4a2c1e8a963f2f7ca4c881ba9e
Ayuka1 <amanda.kanashiro91@gmail.com>
2020-11-17 09:59:06 -0300

    App and index files created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style/App.css'
+import Instructions from './Component/Instructions';
+import EmojiList from './Component/EmojisList'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const greeting= "greeting";
+  const displayAction = false;
+  return(
+    <div className="container">
+      <h1 id={greeting}>Hello world!</h1>
+      {displayAction && <p>I am Writing JSX</p>}
+      <Instructions />
+      <EmojiList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
